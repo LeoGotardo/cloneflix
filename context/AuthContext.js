7818bce7e1f764df647b0b1aecc5b2ff75267352
LeoGotardo <leonardo.gotardo2@gmail.com>
2024-07-12 13:22:53 -0300

    pages almost done

--- context/AuthContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext, useState, useEffect, useContext } from 'react'
-
-const AuthContext = createContext()
-
-export function AuthContextProvider({children}) {
-    <AuthContext.Provider>
-        {children}
-    </AuthContext.Provider>
-}
